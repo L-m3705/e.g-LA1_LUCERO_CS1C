e891e6507d758b17f40214726778887c6bc68d10
L-m3705 <luceromarkeugene399@gmail.com>
2025-03-04 23:21:24 +0800

    Add files via upload

---
+++ booleans.js
@@ -0,0 +1,13 @@
+//Creat a variable named isJsProgrammingLanguage and strore true
+let isJsProgrammingLanguage = true;
+ 
+//Creat a variable named isJshard and store fales
+let isJsHard = false;
+
+//Create variable named favNumb and store your favorite number
+let favNumb = 21
+
+//log all variable to the console
+console.log("Is JS a programming language?", isJsProgrammingLanguage);
+console.log("Is JS Hard?", isJsHard)
+console.log("Favorite Number:", FavNumb);
