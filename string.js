27f4bdc33a07abb8b909f825239660c4e4da88bb
L-m3705 <luceromarkeugene399@gmail.com>
2025-03-04 23:22:44 +0800

    Add files via upload

---
+++ string.js
@@ -0,0 +1,21 @@
+//create varaible name (favActorFirstName) & store your favorite Actor name.
+let favActorFristName = "Daniel"
+
+//create variable name(favAcotrLastName) & store the last name of favorite actor)
+let favActorLastName = "Padilla"
+
+//create varaible name (fullName) & concatenate (favActorFirstName, favActorelastName)
+let fullName = FavActorFirstName +""+ favActorLastName;
+
+//create variable name (uppercase) & store the message about fav actor
+let uppercase = fullName.toUpperCase();
+
+//create a varaible name (message variable) with additional info
+message += ` His best movie is "The hows of as."`;
+
+//log all variable  to the console.
+console.log(`favorite Actor First Name: ${favActorFirstnamw}`);
+console.log(`Favotite Actor Last Name: ${favActorLastName}`);
+console.log(`Full Name: ${fullname}`);
+console.log(`Uppercase Name: ${uppercase}`);
+console.log(`Message: ${message}`);
