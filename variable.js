3e3a6473f90aed5d973ceb316a2451bbdac057f0
L-m3705 <luceromarkeugene399@gmail.com>
2025-03-04 23:19:35 +0800

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,17 @@
+//Creat variable name(name) & store your name
+let name ="Eugene";
+
+//Creat variable name(whatDoYouWAnnabecomeInYourLife) & storen"programming)
+let whatDoYouWAnnabecomeInYourLife = "programming";
+
+//creat variable name (gender)& store your gender
+let gender = 19;
+
+//Creat varaible name (twitterhandle) & store twitterhandle
+let twitterhamdle = "@marky";
+
+//Log all Varaible to the console
+console.log(name);
+console.log(hatDoYouWAnnabecomeInYourLife);
+comsole.log(gender);
+console.log(twitterhandle);
