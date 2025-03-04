013b76d4353b09c74b9ab604de8d7f7fc176e5f1
L-m3705 <luceromarkeugene399@gmail.com>
2025-03-04 23:22:05 +0800

    Add files via upload

---
+++ comparison.js
@@ -0,0 +1,29 @@
+//Create variable name (firstfavNumb) & store your favorite number
+let firstFavNumb = 7;
+
+//create variable name(secondNumb) & store second favorite number
+let secondFavNumber = 21;
+
+//check(firstFavNUmbis greater than secondFavNumb)
+console.log(firstFavNumb > secondFavNumb);
+
+//check (firstFavNumb is less than secondFavNumb)
+console.log(firstFavNumb < secondFavNUmb);
+
+//check (firstFavNumb is greater than & equal to secondFavNumb)
+console.log(firstFavNUmb >= seocndFavNumb);
+
+//check(firstFavNUmb is less then  & equal to secondFavNumb)
+console. log(firstFavNUmber <= secondFavNumb);
+
+//check(firstFavNumb is equal to secondFavNumb) using strick equality operator
+console.log(firstFaNumber === secondFavNumb);
+
+//check(firstFavNumb is equal to secondFavNmb) using loose equality operator
+console.log(firstFavNumb == secondFavNumb);
+
+//check (firstFavNumb is not equal to seconFavNumber) ising strict non-equality operator
+console.log(firtFavNumb !== secondFavNumb);
+
+//check is (firstFavNumb is not equal to secondFavNumb) using loose non-equal
+console.log(firstFavNumb != secondFavNumb)
